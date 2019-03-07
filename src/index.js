2fb8d8dbd287c739a677f5dc2942b9b089d3fee6
Jin Deji <deji.jin@yintech.cn>
2019-03-07 13:31:30 +0800

    load img

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './lufei.png';
 
 function component() {
 	var element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello','webpack'],' ');
 	element.classList.add('hello');
+	// 将图像添加到现有div
+	var myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
+
 	return element;
 }
 
