6618fea7c87b32184f9f6fcee132f394f36f62c4
Jin Deji <deji.jin@yintech.cn>
2019-03-07 17:00:22 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
+import {cube} from './math.js';
 
 function component() {
-	var element = document.createElement('div');
-	var btn = document.createElement('button');
+	// var element = document.createElement('div');
+	var element = document.createElement('pre');
 
-	element.innerHTML = _.join(['Hello','webpack。中文'],' ');
-	btn.innerHTML = 'Click me anb check the console!';
-	btn.onclick = printMe;
-	element.appendChild(btn);
+	element.innerHTML = [
+		'Hello webpack!',
+		'5 cubed is equal to ' + cube(5)
+	].join('\n\n');
 
 	return element;
 }
@@ -18,11 +16,11 @@ function component() {
 let element = component();
 document.body.appendChild(element);
 
-if(module.hot){
-	module.hot.accept('./print.js',function() {
-		console.log('Accepting the updated printMe module!');
-		document.body.removeChild(element);
-		element = component();
-		document.body.appendChild(element);
-	})
-}
+// if(module.hot){
+// 	module.hot.accept('./print.js',function() {
+// 		console.log('Accepting the updated printMe module!');
+// 		document.body.removeChild(element);
+// 		element = component();
+// 		document.body.appendChild(element);
+// 	})
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,35 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: {
-		app: './src/index.js'
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist',
-		hot: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader','css-loader']
-			}
-		]
-	},
-	plugins:[
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			title: 'Output Management'
-		}),
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin()
-	],
+	// entry: {
+	// 	app: './src/index.js'
+	// },
+	entry: './src/index.js',
+	// devtool: 'inline-source-map',
+	// devServer: {
+	// 	contentBase: './dist',
+	// 	hot: true
+	// },
+	// module: {
+	// 	rules: [
+	// 		{
+	// 			test: /\.css$/,
+	// 			use: ['style-loader','css-loader']
+	// 		}
+	// 	]
+	// },
+	// plugins:[
+	// 	new CleanWebpackPlugin(),
+	// 	new HtmlWebpackPlugin({
+	// 		title: 'Output Management'
+	// 	}),
+	// 	new webpack.NamedModulesPlugin(),
+	// 	new webpack.HotModuleReplacementPlugin()
+	// ],
 	output: {
-		filename: '[name].bundle.js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname,'dist')
 		// publicPath: '/'
 	},
+	mode: "production"
 };
