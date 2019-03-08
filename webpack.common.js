7bcde9b20b18ebe218ab3b212bc2ab2d35d76c0e
Jin Deji <deji.jin@yintech.cn>
2019-03-08 14:28:18 +0800

    生产环境构建

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/index.js'
+	},
+	// devtool: 'inline-source-map',
+	// devServer: {
+	// 	contentBase: './dist',
+	// 	hot: true
+	// },
+	module: {
+		rules: [
+			// {
+			// 	test: /\.css$/,
+			// 	use: ['style-loader','css-loader']
+			// }
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015']
+				}
+			}
+		]
+	},
+	plugins:[
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Production'
+		})
+		
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname,'dist')
+		// publicPath: '/'
+	}
+};
